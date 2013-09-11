ee6c91975a9f0c7860234cebbd28fe6d41d3d644
Marco Palermo <fittiuw@gmail.com>
2013-09-11 17:39:24 +0200

    credits

--- move-to-subsite.php
+++ move-to-subsite.php
@@ -1,12 +1,34 @@
 <?php
-
 /*
   Plugin Name: Move to Subsite
-  Description: Move posts in a category and/or a page hierarchy to a new subsite, with seamless redirects.
-  Version: 0.1
-  Author: Matt Wiebe
-  Author URI: http://mattwiebe.wordpress.com/
- */
+  Description: Move posts in a category and/or a page hierarchy to a new subsite, with seamless redirects. It move also attachments.
+  Version: 0.2
+  Author: Marco Palermo 
+  Author URI: http://www.mediaprime.it/
+  Plugin URI: https://github.com/palermomarco/move-to-subsite
+  
+  Forked from the work of Matt Wiebe (http://wp.mattwie.be) at http://wordpress.org/plugins/move-to-subsite/
+
+== Copyright ==
+Copyright 2013 Marco Palermo (email: marco@mediaprime.it)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
 
 class MW_Move_Base {
 
